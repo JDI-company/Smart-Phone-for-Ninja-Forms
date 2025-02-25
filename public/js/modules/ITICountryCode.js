641e77a061ef3abbd355f770f674e7480d329804
GooodGod <st.38425@gmail.com>
2025-02-25 22:38:04 +0100

    Added data element to field and fixed naming

--- public/js/modules/ITICountryCode.js
+++ public/js/modules/ITICountryCode.js
@@ -47,7 +47,7 @@ class ITICountryCode {
         countryCode = $wrapper.find('.iti__selected-flag').text().match(/[+\d]+/g)?.join('')
       }
 
-      const exclude = $wrapper.find('input[type="tel"]').data('excludeCountrycodeFromSubmission');
+      const exclude = $wrapper.find('input[type="tel"]#nf-field-' + modelID).data('excludeCountryCodeFromSubmission');
 
       if(exclude !== "1"){
         this.$phoneHidden.val(countryCode + $phone.val())
